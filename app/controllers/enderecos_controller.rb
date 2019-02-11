94ae9de0be51dbd5752f1964e9313b22f618ae96
Pedro Augusto <pedroaugustovitor@hotmail.com>
2019-02-11 17:02:15 +0000

    Criado gets pra pais, estado

--- app/controllers/enderecos_controller.rb
+++ app/controllers/enderecos_controller.rb
@@ -1,12 +1,23 @@
 class EnderecosController < ApplicationController
   def association
     @unidades = Unidade.all
-    render json: @unidades.to_json(include: {estados: {include: :cidades,:except => ['created_at', 'updated_at']}},:except => ['created_at', 'updated_at'])
+    render json: @unidades.to_json(include: {estados: {include: :cidades, :except => [:created_at, :updated_at]}},:except => [:created_at, :updated_at])
+    #render json: @unidades.to_json(include: {estados: {include: :cidades, :except => [:created_at, :updated_at]}},:except => [:created_at, :updated_at])
   end
   
   def paises
     @unidades = Unidade.all
     render json: @unidades.to_json(:except => ['created_at', 'updated_at'])
+  end
+  
+  def pais
+    pais = Unidade.find(params[:id])
+    render json: pais.to_json(include: {estados: {:except => [:created_at, :updated_at]}}, :except => [:created_at, :updated_at])
+  end
+  
+  def estado
+    estado = Estado.find(params[:id])
+    render json: estado.to_json(include: {cidades: {:except => [:created_at, :updated_at]}}, :except => [:created_at, :updated_at])
   end
   
   def estados
