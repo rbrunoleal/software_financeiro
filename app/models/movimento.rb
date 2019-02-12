3ad08f671bfe15592c3142c27ce398c7fa1c8ed3
Bruno Leal <robertobruno.leal@hotmail.com>
2019-02-12 13:54:44 +0000

    Ajustes Movimentos

--- app/models/movimento.rb
+++ app/models/movimento.rb
@@ -2,6 +2,7 @@ class Movimento < ApplicationRecord
     belongs_to :conta
     belongs_to :pessoa
     belongs_to :nota, optional: true
+    accepts_nested_attributes_for :nota, reject_if: :all_blank, allow_destroy: true
     
     def favorecido
       self.pessoa.nome
