94c5ed5532b4d4c6ee944d9b4bfdf8295cccc0d2
Pedro Augusto <pedroaugustovitor@hotmail.com>
2019-02-14 13:19:09 +0000

    Corrige erro de navegação entre páginas

--- app/javascript/packs/favorecidos.js
+++ app/javascript/packs/favorecidos.js
@@ -23,167 +23,164 @@ Vue.use(Toastr, {
 
 Vue.use(BootstrapVue);
 
+axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+const favorecidosApp = new Vue({
+  el: document.getElementById('favorecidosApp'),
+  data: {
+    dialog: false,
+    loading: true,
+    create: false,
+    contato: {},
+    clickedFavorecido: {
+      contatos: [],
+      endereco: {},
+      pessoafisica: {},
+      pessoajuridica: {},
+    },
+    favorecidos: [],
+    showModal: false,
+    allSelected: false,
+    show: false,
+    paises: [],
+    pais: {estados: []},
+    estado: {cidades: []},
+    step: 5,
 
-window.addEventListener('turbolinks:load', function () {
-  axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-  const favorecidosApp = new Vue({
-    el: document.getElementById('favorecidosApp'),
-    data: {
-      dialog: false,
-      loading: true,
-      create: false,
-      contato: {},
-      clickedFavorecido: {
+  },
+  mounted () {
+    this.searchFavorecidos();
+    this.setPaises();
+    
+  },
+  methods: {
+    mountCreateForm: function () {
+      this.$refs.formFavorecidoModal.show();
+      this.create = true;
+      this.pais = [];
+      this.estado = [];
+      this.clickedFavorecido = {
         contatos: [],
         endereco: {},
         pessoafisica: {},
         pessoajuridica: {},
-      },
-      favorecidos: [],
-      showModal: false,
-      allSelected: false,
-      show: false,
-      paises: [],
-      pais: {estados: []},
-      estado: {cidades: []},
-      step: 5,
-
+      };
     },
-    mounted () {
-      this.searchFavorecidos();
-      this.setPaises();
-      
+    mountDeleteForm: function (favorecido) {
+      this.$refs.deleteFavorecidoModal.show();
+      this.clickedFavorecido = favorecido;
     },
-    methods: {
-      mountCreateForm: function () {
-        this.$refs.formFavorecidoModal.show();
-        this.create = true;
-        this.pais = [];
-        this.estado = [];
-        this.clickedFavorecido = {
-          contatos: [],
-          endereco: {},
-          pessoafisica: {},
-          pessoajuridica: {},
-        };
-      },
-      mountDeleteForm: function (favorecido) {
-        this.$refs.deleteFavorecidoModal.show();
-        this.clickedFavorecido = favorecido;
-      },
-      mountEditForm: function (favorecido) {
-        this.$refs.formFavorecidoModal.show();
-        this.setPais(favorecido.endereco.unidade_id);
-        this.setEstado(favorecido.endereco.estado_id);
-        this.create = false;
-        favorecido.tipo === "Física"? this.step = 2: this.step = 3;
-        favorecido.pessoaFisica ? this.clickedFavorecido = {... favorecido, pessoafisica: {}} : this.clickedFavorecido = {... favorecido, pessoajuridica: {}};
-        this.setPais(this.clickedFavorecido.endereco.unidade_id);
-        this.setEstado(this.clickedFavorecido.endereco.estado_id);
-      },
-      deleteFavorecido: function (id){
-        this.loading = true;
-        axios
-            .delete(`${URL}/pessoas/${id}.json`)
-            .then(response => {
-              this.searchFavorecidos();
-              this.$toastr.s("Registro apagado.");
-              this.$refs.deleteFavorecidoModal.hide();
-            })
-            .catch(error => {
-              this.$toastr.e("Não foi possível excluir")
-            })
-            .finally(() => this.loading = false)
-      },
-      searchFavorecidos: function(){
-        this.loading = true;
-        this.clickedFavorecido = {
-          contatos: [],
-          endereco: {},
-          pessoafisica: {},
-          pessoajuridica: {},
+    mountEditForm: function (favorecido) {
+      this.$refs.formFavorecidoModal.show();
+      this.setPais(favorecido.endereco.unidade_id);
+      this.setEstado(favorecido.endereco.estado_id);
+      this.create = false;
+      favorecido.tipo === "Física"? this.step = 2: this.step = 3;
+      favorecido.pessoaFisica ? this.clickedFavorecido = {... favorecido, pessoafisica: {}} : this.clickedFavorecido = {... favorecido, pessoajuridica: {}};
+      this.setPais(this.clickedFavorecido.endereco.unidade_id);
+      this.setEstado(this.clickedFavorecido.endereco.estado_id);
+    },
+    deleteFavorecido: function (id){
+      this.loading = true;
+      axios
+          .delete(`${URL}/pessoas/${id}.json`)
+          .then(response => {
+            this.searchFavorecidos();
+            this.$toastr.s("Registro apagado.");
+            this.$refs.deleteFavorecidoModal.hide();
+          })
+          .catch(error => {
+            this.$toastr.e("Não foi possível excluir")
+          })
+          .finally(() => this.loading = false)
+    },
+    searchFavorecidos: function(){
+      this.loading = true;
+      this.clickedFavorecido = {
+        contatos: [],
+        endereco: {},
+        pessoafisica: {},
+        pessoajuridica: {},
 
-        };
+      };
 
-        axios
-            .get(`${URL}/pessoas.json`)
-            .then(response => {
-              this.favorecidos = response.data;
-              this.clickedFavorecido.pessoajuridica = {}
-            })
-            .catch(error => {
-              this.errored = true
-            })
-            .finally(() => this.loading = false)
-      },
-      createFavorecido: function(favorecido){
-        let pessoa = {tipo: favorecido.tipo, pessoafisica_attributes: favorecido.pessoafisica, endereco_attributes: favorecido.endereco, contatos_attributes: favorecido.contatos };
-        this.loading = true;
-        pessoa.tipo === "Física"? pessoa.pessoajuridica = null: pessoa.pessoafisica = null;
-        axios
-            .post(`${URL}/pessoas.json`, {pessoa})
-            .then(response => {
-              this.$refs.formFavorecidoModal.hide();
-              this.searchFavorecidos();
-              this.$toastr.s("Registro criado.");
-            })
-            .catch(error => {
-              this.$toastr.e("Não foi possível adicionar.");
-            })
-            .finally(() => this.loading = false)
-      },
-      updateFavorecido: function(favorecido){
-        this.loading = true;
-        let pessoa = {id: favorecido.id, tipo: favorecido.tipo, pessoafisica_attributes: favorecido.pessoafisica, endereco_attributes: favorecido.endereco, contatos_attributes: favorecido.contatos };
-        pessoa.tipo === "Física" ? pessoa.pessoajuridica = null: pessoa.pessoafisica = null;
-        axios
-            .put(`${URL}/pessoas/${pessoa.id}.json`, {pessoa})
-            .then(response => {
-              this.$refs.formFavorecidoModal.hide();
-              this.searchFavorecidos();
-              this.$toastr.s("Registro atualizado.");
-            })
-            .catch(error => {
-              this.$toastr.e("Não foi possível adicionar.");
-            })
-            .finally(() => this.loading = false)
-      },
-      selectAll: function() {
-        this.allSelected ? this.favorecidos.map( favorecido  => favorecido.selected = false) : this.favorecidos.map( favorecido  => favorecido.selected = true);
-      },
-      select: function() {
-        this.allSelected = false;
-      },
-      closeModal(){
-        this.$refs.deleteFavorecidoModal.hide();
-        this.$refs.formFavorecidoModal.hide()
-      },
-      setPaises(){
-        this.loading = true;
-        axios.get(`${URL}/enderecos/paises.json`).then(response => {this.paises = response.data});
-      },
-      setPais(pais_id){
-        this.loading = true;
-        axios.get(`${URL}/enderecos/${pais_id}/pais.json`).then(response => {this.pais = response.data; this.loading = false; this.estado = {}});
-      },
-      setEstado(estado_id){
-        this.loading = true;
-        axios.get(`${URL}/enderecos/${estado_id}/estado.json`).then(response => {this.estado = response.data; this.loading = false});
-      },
-      addContato(){
-        this.clickedFavorecido.contatos.push(this.contato);
-        this.contato = {}
-      },
-      editContato(index){
-        this.contato = this.clickedFavorecido.contatos[index];
-        this.clickedFavorecido.contatos.splice(index, 1);
-      },
-      deleteContato(index){
-        this.$set(this.clickedFavorecido.contatos[index], '_destroy', true);
-      },
-      undoContato(index){
-        this.clickedFavorecido.contatos[index]._destroy = false;
-      }
+      axios
+          .get(`${URL}/pessoas.json`)
+          .then(response => {
+            this.favorecidos = response.data;
+            this.clickedFavorecido.pessoajuridica = {}
+          })
+          .catch(error => {
+            this.errored = true
+          })
+          .finally(() => this.loading = false)
+    },
+    createFavorecido: function(favorecido){
+      let pessoa = {tipo: favorecido.tipo, pessoafisica_attributes: favorecido.pessoafisica, endereco_attributes: favorecido.endereco, contatos_attributes: favorecido.contatos };
+      this.loading = true;
+      pessoa.tipo === "Física"? pessoa.pessoajuridica = null: pessoa.pessoafisica = null;
+      axios
+          .post(`${URL}/pessoas.json`, {pessoa})
+          .then(response => {
+            this.$refs.formFavorecidoModal.hide();
+            this.searchFavorecidos();
+            this.$toastr.s("Registro criado.");
+          })
+          .catch(error => {
+            this.$toastr.e("Não foi possível adicionar.");
+          })
+          .finally(() => this.loading = false)
+    },
+    updateFavorecido: function(favorecido){
+      this.loading = true;
+      let pessoa = {id: favorecido.id, tipo: favorecido.tipo, pessoafisica_attributes: favorecido.pessoafisica, endereco_attributes: favorecido.endereco, contatos_attributes: favorecido.contatos };
+      pessoa.tipo === "Física" ? pessoa.pessoajuridica = null: pessoa.pessoafisica = null;
+      axios
+          .put(`${URL}/pessoas/${pessoa.id}.json`, {pessoa})
+          .then(response => {
+            this.$refs.formFavorecidoModal.hide();
+            this.searchFavorecidos();
+            this.$toastr.s("Registro atualizado.");
+          })
+          .catch(error => {
+            this.$toastr.e("Não foi possível adicionar.");
+          })
+          .finally(() => this.loading = false)
+    },
+    selectAll: function() {
+      this.allSelected ? this.favorecidos.map( favorecido  => favorecido.selected = false) : this.favorecidos.map( favorecido  => favorecido.selected = true);
+    },
+    select: function() {
+      this.allSelected = false;
+    },
+    closeModal(){
+      this.$refs.deleteFavorecidoModal.hide();
+      this.$refs.formFavorecidoModal.hide()
+    },
+    setPaises(){
+      this.loading = true;
+      axios.get(`${URL}/enderecos/paises.json`).then(response => {this.paises = response.data});
+    },
+    setPais(pais_id){
+      this.loading = true;
+      axios.get(`${URL}/enderecos/${pais_id}/pais.json`).then(response => {this.pais = response.data; this.loading = false; this.estado = {}});
+    },
+    setEstado(estado_id){
+      this.loading = true;
+      axios.get(`${URL}/enderecos/${estado_id}/estado.json`).then(response => {this.estado = response.data; this.loading = false});
+    },
+    addContato(){
+      this.clickedFavorecido.contatos.push(this.contato);
+      this.contato = {}
+    },
+    editContato(index){
+      this.contato = this.clickedFavorecido.contatos[index];
+      this.clickedFavorecido.contatos.splice(index, 1);
+    },
+    deleteContato(index){
+      this.$set(this.clickedFavorecido.contatos[index], '_destroy', true);
+    },
+    undoContato(index){
+      this.clickedFavorecido.contatos[index]._destroy = false;
     }
-  })
-});
+  }
+})
--- app/javascript/packs/movimentos.js
+++ app/javascript/packs/movimentos.js
@@ -21,141 +21,140 @@ Vue.use(Toastr, {
 });
 Vue.use(BootstrapVue);
 
-window.addEventListener('turbolinks:load', function () {
-  axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-    const movimentosIndex = new Vue({
-      el: document.getElementById('movimentosApp'),
-      data: {
-        loading: true,
-        create: false,
-        clickedMovimento: {pessoa: {}, conta: {}, nota: {}},
-        movimentos: [],
-        pickedMovimento: {},
-        showModal: false,
-        allSelected: false,
-        show: false,
-        pessoas: [],
-        contas: {},
-        nota: false
-      },
-      mounted(){
-        this.searchMovimentos();
-        axios.get(`${URL}/contas.json`).then(response => {this.contas = response.data});
-        axios.get(`${URL}/pessoas.json`).then(response => {this.pessoas = response.data});
-      },
-      methods: { 
-        DataFormatada (data) {
-          var date = new Date(data);
-          var dataformatada = date.toLocaleDateString("pt-BR");
-          return `${dataformatada}`
-        },
-        ContaFormatada (contanumero,contadigito){
-          return `${contanumero} - ${contadigito}`
-        },
-        NotaFormatada (nota){
-          if(nota != null){
-            return `${nota.numero} - ${nota.descricao}`
-          }
-        },
-        mountCreateForm: function () {
-          this.$refs.formMovimentoModal.show();
-          this.create = true;
-          this.clickedMovimento = {pessoa: {}, conta: {}, nota: {}};
-        },
-        mountDeleteForm: function (movimento) {
-          this.$refs.deleteMovimentoModal.show();
-          this.clickedMovimento = movimento;
-        },
-        mountEditForm: function (movimento) {
-          this.$refs.formMovimentoModal.show();
-          this.create = false;
-          this.clickedMovimento = {... movimento};
-        },
-        deleteMovimento: function (id){
-          axios
-            .delete(`${URL}/movimentos/${id}.json`)
-            .then(response => {
-              this.searchMovimentos();
-              this.$toastr.s("Registro apagado.");
-              this.$refs.deleteMovimentoModal.hide();
-            })
-            .catch(error => {
-              this.$toastr.e("Não foi possível excluir")
-            })
-            .finally(() => this.loading = false)
-        },
-        searchMovimentos: function(){
-          this.loading = true;
-          this.clickedMovimento = {pessoa: {}, conta: {}, nota: {}};
-          axios
-            .get(`${URL}/movimentos.json`)
-            .then(response => {
-              this.movimentos = response.data
-            })
-            .catch(error => {
-              this.errored = true
-            })
-              .finally(() => this.loading = false)
-        },
-        createMovimento: function(lmovimento){
-          let movimento = {
-            data_competencia: lmovimento.data_competencia,
-            data_vencimento: lmovimento.data_vencimento,
-            descricao: lmovimento.descricao,
-            valor: lmovimento.valor,
-            conta_id: lmovimento.conta_id,
-            pessoa_id: lmovimento.pessoa.id,
-            nota_attributes: lmovimento.nota
-          };
-        this.loading = true;
-          axios.post(`${URL}/movimentos.json`, {
-            movimento
-          })
-          .then(response => {
-              this.$refs.formMovimentoModal.hide();
-              this.searchMovimentos();
-              this.$toastr.s("Registro criado.");
-            })
-            .catch(error => {
-              this.$toastr.e("Não foi possível adicionar.");
-            })
-              .finally(() => this.loading = false)
-        },
-        updateMovimento: function(lmovimento){
-          this.loading = true;
-          let movimento = {
-            id: lmovimento.id,
-            data_competencia: lmovimento.data_competencia,
-            data_vencimento: lmovimento.data_vencimento,
-            descricao: lmovimento.descricao,
-            valor: lmovimento.valor,
-            conta_id: lmovimento.conta_id,
-            pessoa_id: lmovimento.pessoa_id.id,
-            nota_attributes: lmovimento.nota
-          };
-          axios.put(`${URL}/movimentos/${movimento.id}.json`, {
-            movimento
-          })
-          .then(response => {
-              this.$refs.formMovimentoModal.hide();
-              this.searchMovimentos();
-              this.$toastr.s("Registro atualizado.");
-          })
-          .catch(error => {
-            this.$toastr.e("Não foi possível adicionar.");
-          })
-          .finally(() => this.loading = false)
-        },
-        selectAll: function() {
-          this.allSelected ? this.movimentos.map( movimento  => movimento.selected = false) : this.movimentos.map( movimento  => movimento.selected = true);
-        },
-        select: function() {
-          this.allSelected = false;
-        },
-        closeModal(){
-          this.$refs.deleteMovimentoModal.hide();
-          this.$refs.formMovimentoModal.hide()
-        }
+
+axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+const movimentosIndex = new Vue({
+  el: document.getElementById('movimentosApp'),
+  data: {
+    loading: true,
+    create: false,
+    clickedMovimento: {pessoa: {}, conta: {}, nota: {}},
+    movimentos: [],
+    pickedMovimento: {},
+    showModal: false,
+    allSelected: false,
+    show: false,
+    pessoas: [],
+    contas: {},
+    nota: false
+  },
+  mounted(){
+    this.searchMovimentos();
+    axios.get(`${URL}/contas.json`).then(response => {this.contas = response.data});
+    axios.get(`${URL}/pessoas.json`).then(response => {this.pessoas = response.data});
+  },
+  methods: { 
+    DataFormatada (data) {
+      var date = new Date(data);
+      var dataformatada = date.toLocaleDateString("pt-BR");
+      return `${dataformatada}`
+    },
+    ContaFormatada (contanumero,contadigito){
+      return `${contanumero} - ${contadigito}`
+    },
+    NotaFormatada (nota){
+      if(nota != null){
+        return `${nota.numero} - ${nota.descricao}`
       }
-    })
-}) 
+    },
+    mountCreateForm: function () {
+      this.$refs.formMovimentoModal.show();
+      this.create = true;
+      this.clickedMovimento = {pessoa: {}, conta: {}, nota: {}};
+    },
+    mountDeleteForm: function (movimento) {
+      this.$refs.deleteMovimentoModal.show();
+      this.clickedMovimento = movimento;
+    },
+    mountEditForm: function (movimento) {
+      this.$refs.formMovimentoModal.show();
+      this.create = false;
+      this.clickedMovimento = {... movimento};
+    },
+    deleteMovimento: function (id){
+      axios
+        .delete(`${URL}/movimentos/${id}.json`)
+        .then(response => {
+          this.searchMovimentos();
+          this.$toastr.s("Registro apagado.");
+          this.$refs.deleteMovimentoModal.hide();
+        })
+        .catch(error => {
+          this.$toastr.e("Não foi possível excluir")
+        })
+        .finally(() => this.loading = false)
+    },
+    searchMovimentos: function(){
+      this.loading = true;
+      this.clickedMovimento = {pessoa: {}, conta: {}, nota: {}};
+      axios
+        .get(`${URL}/movimentos.json`)
+        .then(response => {
+          this.movimentos = response.data
+        })
+        .catch(error => {
+          this.errored = true
+        })
+          .finally(() => this.loading = false)
+    },
+    createMovimento: function(lmovimento){
+      let movimento = {
+        data_competencia: lmovimento.data_competencia,
+        data_vencimento: lmovimento.data_vencimento,
+        descricao: lmovimento.descricao,
+        valor: lmovimento.valor,
+        conta_id: lmovimento.conta_id,
+        pessoa_id: lmovimento.pessoa.id,
+        nota_attributes: lmovimento.nota
+      };
+    this.loading = true;
+      axios.post(`${URL}/movimentos.json`, {
+        movimento
+      })
+      .then(response => {
+          this.$refs.formMovimentoModal.hide();
+          this.searchMovimentos();
+          this.$toastr.s("Registro criado.");
+        })
+        .catch(error => {
+          this.$toastr.e("Não foi possível adicionar.");
+        })
+          .finally(() => this.loading = false)
+    },
+    updateMovimento: function(lmovimento){
+      this.loading = true;
+      let movimento = {
+        id: lmovimento.id,
+        data_competencia: lmovimento.data_competencia,
+        data_vencimento: lmovimento.data_vencimento,
+        descricao: lmovimento.descricao,
+        valor: lmovimento.valor,
+        conta_id: lmovimento.conta_id,
+        pessoa_id: lmovimento.pessoa_id.id,
+        nota_attributes: lmovimento.nota
+      };
+      axios.put(`${URL}/movimentos/${movimento.id}.json`, {
+        movimento
+      })
+      .then(response => {
+          this.$refs.formMovimentoModal.hide();
+          this.searchMovimentos();
+          this.$toastr.s("Registro atualizado.");
+      })
+      .catch(error => {
+        this.$toastr.e("Não foi possível adicionar.");
+      })
+      .finally(() => this.loading = false)
+    },
+    selectAll: function() {
+      this.allSelected ? this.movimentos.map( movimento  => movimento.selected = false) : this.movimentos.map( movimento  => movimento.selected = true);
+    },
+    select: function() {
+      this.allSelected = false;
+    },
+    closeModal(){
+      this.$refs.deleteMovimentoModal.hide();
+      this.$refs.formMovimentoModal.hide()
+    }
+  }
+})
