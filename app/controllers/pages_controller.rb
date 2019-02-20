d30ef0f20949fb5a0aa56521f2a45c5942be3ef9
Pedro Augusto <pedroaugustovitor@hotmail.com>
2019-02-20 12:03:16 +0000

    Remove buscas desnecessárias

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,9 @@
 class PagesController < ApplicationController
   before_action :authenticate_usuario!, :except => [:index, :contact]
   def welcome
-    @contas = Conta.all
   end
 
   def index
-    @contas = Conta.all
   end
 
   def contact
