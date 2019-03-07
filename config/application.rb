fa02e2391a9a3d4e2e3a95e7b8dde41e59e71706
Pedro Augusto <pedroaugustovitor@hotmail.com>
2019-03-07 15:16:02 +0000

    Setando precompile false

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module Projeto
     config.load_defaults 5.2
     config.i18n.default_locale = :'pt-BR'
     config.time_zone = 'Brasilia'
-
+    
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
